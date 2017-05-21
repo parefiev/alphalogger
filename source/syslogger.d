64cbf7384dab3b85d48a90d8b47bcd09ff16cd8b
Pavel Arefiev <pavel.arefiev.c@yandex.ru>
2017-05-21 19:26:10 +0300

    Add static SysLogger: this basically is not for this project, but can be a convenient single-entry point for project-wide system logging tasks. You should inject loggers where it's possible, of course.

---
+++ source/syslogger.d
@@ -0,0 +1,60 @@
+import ilogger;
+import loglevel;
+import ttylogger;
+
+static class SysLogger
+{
+	protected static ILogger l;
+
+	protected static this()
+	{
+		l = new TtyLogger();
+	}
+
+	public:
+		static void info(string message)
+		{
+			log(LogLevel.INFO, message);
+		}
+
+		static void debug_(string message)
+		{
+			log(LogLevel.DEBUG, message);
+		}
+
+		static void emergency(string message)
+		{
+			log(LogLevel.EMERGENCY, message);
+		}
+
+		static void alert(string message)
+		{
+			log(LogLevel.ALERT, message);
+		}
+
+		static void critical(string message)
+		{
+			log(LogLevel.CRITICAL, message);
+		}
+
+		static void error(string message)
+		{
+			log(LogLevel.ERROR, message);
+		}
+
+		static void warning(string message)
+		{
+			log(LogLevel.WARNING, message);
+		}
+
+		static void notice(string message)
+		{
+			log(LogLevel.NOTICE, message);
+		}
+
+		static void log(LogLevel logLevel, string m)
+		{
+			l.log(logLevel, m);
+		}
+}
+
